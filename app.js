7bd838c1603aa1f7d81b318ebfd576a5b69d23e3
liangjiapei1103 <liang98@purdue.edu>
2016-01-17 18:05:53 -0500

    Adding comments to app.js and bin/www

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// require all needed modules
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,21 +6,28 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// require all needed .js
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// create an instance of express
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// set folder 'views' as the place to store all the view templates
+app.set('views', path.join(__dirname, 'views')); 
+// set the template engine as ejs
+app.set('view engine', 'ejs'); 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// using all the needed middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// set the folder 'public' to store static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -56,5 +64,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+// exports app to let other module to use
 module.exports = app;
