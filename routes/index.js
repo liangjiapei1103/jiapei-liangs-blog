33a291b0216f43f4a5bb107ac9d72e76e9919d72
liangjiapei1103 <liang98@purdue.edu>
2016-01-18 02:18:20 -0500

    edit index.js and app.get('/) and app.get('/register')

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var crypto = require('crypto'),
+	User = require('../models/user.js');
+
+
+module.exports = function(app) {
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jiapei Liangs Blog' });
-});
+	app.get('/', function (req, res, next) {
+	  res.render('index', { 
+	  	title: 'Jiapei Liangs Blog',
+	  	user: req.session.user,
+	  	success: req.flash('success').toString(),
+	  	error: req.flash('error').toString()
+	  });
+	});
+
+	app.get('/register', function (req, res) {
+		res.render('register', {
+			title: '注册',
+			user: req.session.user,
+	  		success: req.flash('success').toString(),
+	  		error: req.flash('error').toString()
+		});
+	});
+
+};
+
+
 
 module.exports = router;
